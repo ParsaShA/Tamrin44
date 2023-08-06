ddb6a6919c0b17cfe5158068ad1c199b91ac4bf7
parsa <parsa.shakibaei.economy401@gmail.com>
2023-08-06 06:02:23 -0700

    tamrin3 takmil shod

---
+++ Tamrin4.py
@@ -0,0 +1,14 @@
+lst1  = [1,2,3,4]
+lst2 = [4,5,6,7]
+
+
+def finder(lst1,lst2):
+    for i in lst1:
+        if i not in lst2:
+            print(i , end =' ')
+    for j in lst2:
+        if j not in lst1:
+            print (j, end = ' ')
+    
+    
+(finder(lst1,lst2))

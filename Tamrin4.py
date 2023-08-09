8d23ee8b97a5caaedf33aa784a92d627b21bcf64
parsa <parsa.shakibaei.economy401@gmail.com>
2023-08-09 00:34:06 -0700

    tamrin4 tamam v kamal ok shod

--- Tamrin4.py
+++ Tamrin4.py
@@ -1,8 +1,9 @@
+
 lst1  = [1,2,3,4]
 lst2 = [4,5,6,7]
 
 
-def finder(lst1,lst2):
+def finder_odd(lst1,lst2):
     for i in lst1:
         if i not in lst2:
             print(i , end =' ')
@@ -11,4 +12,4 @@ def finder(lst1,lst2):
             print (j, end = ' ')
     
     
-(finder(lst1,lst2))
+(finder_odd(lst1,lst2))
